71d5324fdfb56d8a6de0311448c5799567a37b36
Nir Beja <90895452+Noogra@users.noreply.github.com>
2024-07-13 15:00:23 +0300

    gitignore

--- app/src/main/java/com/example/firstproject/App.java
+++ app/src/main/java/com/example/firstproject/App.java
@@ -8,13 +8,10 @@ import com.example.firstproject.Utilities.SignalManager;
 
 
 public class App extends Application {
-    //SharedPreferencesManagerV3 sharedPreferencesManagerV3;
     @Override
     public void onCreate(){
         super.onCreate();
         SharedPreferencesManagerV3.init(this);
-        /*sharedPreferencesManagerV3 = SharedPreferencesManagerV3.getInstance();
-        sharedPreferencesManagerV3.removeEntry("highScores");*/
         SignalManager.init(this);
 
     }
